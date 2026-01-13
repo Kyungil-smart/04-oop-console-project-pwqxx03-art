62b93ca3c04c5a451af89df1744c22a177137917
pwqxx03-art <pwqxx03@gmail.com>
2026-01-13 09:53:41 +0900

    Update RandomBox.cs

--- ConsoleProject/ConsoleProject/GameObjects/RandomBox.cs
+++ ConsoleProject/ConsoleProject/GameObjects/RandomBox.cs
@@ -1,3 +1,16 @@
 using System;
 using System.Threading;
 
+public class RandomBox : Item
+{
+    public RandomBox()
+    {
+        Symbol = "★"; 
+        Name = "의문의 상자";
+    }
+
+    public override void Use()
+    {
+         Console.Clear();
+         Console.WriteLine("\n\n");
+         Console.WriteLine("반짝이는 아이템상자를 여는증...");
